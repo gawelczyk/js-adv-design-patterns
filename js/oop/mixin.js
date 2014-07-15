e2bf2429599b1fc3beea4cf029b6921e634777ee
Adam Gawelczyk <adam.gawelczyk@motorolasolutions.com>
2014-07-15 16:53:25 +0200

    second day/ afternoon mixin pattern in use

--- js/oop/mixin.js
+++ js/oop/mixin.js
@@ -1,41 +1,73 @@
 // Implement mixin(destination, source) function.
+function mixin(destination, source) {
+
+    for (var prop in source) {
+        destination[prop] = source[prop];
+    }
+
+}
+
+var Encryptable = (function () {
+
+    var En = {
+        encrypt: function (propertyName) {
+            console.log('encrypt', propertyName);
+        },
+        getEncrypted: function (propertyName) {
+            console.log('getEncrypted', propertyName);
+        }
+    }
+    return En;
+})();
 
 function mixinClassExampleUsage(mixin) {
-  function User(login) {
-    this.login = login;
-  }
+    function User(login) {
+        this.login = login;
+    }
 
-  User.prototype.getLogin = function() {
-    return this.login;
-  };
+    User.prototype.getLogin = function () {
+        return this.login;
+    };
 
-  // TODO: create Encryptable module (Singleton) with methods:
-  // + encryp t(propertyName)
-  // + getEncrypted(propertyName)
-  mixin(User.prototype, Encryptable);
+    // TODO: create Encryptable module (Singleton) with methods:
+    // + encrypt(propertyName)
+    // + getEncrypted(propertyName)
 
-  var user = new User("bob");
-  user.encrypt("login"); // => "some encrypted login"
-  user.getEncrypted("login");
+    mixin(User.prototype, Encryptable);
+
+    var user = new User("bob");
+    user.encrypt("login"); // => "some encrypted login"
+    user.getEncrypted("login");
 }
-//mixinClassExampleUsage(mixin);
+
+console.log('mixinClassExampleUsage');
+mixinClassExampleUsage(mixin);
+console.log('//mixinClassExampleUsage');
 
 function mixinInstanceExampleUsage(mixin) {
-  var bob = {name: "Bob"};
+    var bob = {name: "Bob"};
 
-  // TODO: create modules (Singletons):
-  // + Teacher with methods: teach()
-  // + Dancer with methods: dance()
-  var Teacher = {
-    teach: function() {
-      console.log("teach");
-    }
-  };
+    // TODO: create modules (Singletons):
+    // + Teacher with methods: teach()
+    // + Dancer with methods: dance()
+    var Teacher = {
+        teach: function () {
+            console.log("teach");
+        }
+    };
 
-  mixin(bob, Teacher);
-  mixin(bob, Dancer);
+    var Dancer = {
+        dance: function () {
+            console.log('dance');
+        }
+    };
 
-  bob.teach();
-  bob.dance(); // logs "dance"
+    mixin(bob, Teacher);
+    mixin(bob, Dancer);
+
+    bob.teach();
+    bob.dance(); // logs "dance"
 }
-//mixinInstanceExampleUsage(mixin);
+console.log('mixinInstanceExampleUsage test');
+mixinInstanceExampleUsage(mixin);
+console.log('//mixinInstanceExampleUsage test');
