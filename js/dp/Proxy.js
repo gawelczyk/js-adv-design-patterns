ff770da69ffca807c9f07f2a4f4ddd91a0cf27ba
Adam Gawelczyk <adam.gawelczyk@motorolasolutions.com>
2014-07-16 21:13:58 +0200

    third day work, proxy and stateful

--- js/dp/Proxy.js
+++ js/dp/Proxy.js
@@ -1,27 +1,108 @@
 /*
-* Using userRepository create cachedUserRepository -
-* it should cache results for up to 5 seconds, and send AJAX
-* request only if there are no cached results or cache has expired
-* */
+ * Using userRepository create cachedUserRepository -
+ * it should cache results for up to 5 seconds, and send AJAX
+ * request only if there are no cached results or cache has expired
+ * */
+
+var cachedUserRepository = (function (repo) {
+    var TIMEOUT = 5000;
+    var allData;
+    var byIdData = {};
+
+    var findAll = function (fn) {
+        if (!allData) {
+            console.log('fetching data');
+            repo.findAll(function (data) {
+                allData = data;
+                setTimeout(function () {
+                    allData = null;
+                }, TIMEOUT);
+                fn(allData);
+            });
+        } else {
+            console.log('cache used');
+            fn(allData);
+        }
+    };
+
+    var findById = function (id, fn1) {
+        if (!byIdData[id]) {
+            console.log('fetching data', byIdData[id]);
+            repo.findById(id, function (data) {
+                byIdData[id] = data;
+                setTimeout(function () {
+                    byIdData[id] = null;
+                }, TIMEOUT);
+                fn1(byIdData[id]);
+            });
+        } else {
+            console.log('cache used', byIdData[id]);
+            fn1(byIdData[id]);
+        }
+    };
+
+    return {
+        findAll: findAll,
+        findById: findById
+    }
+})(UsersRepository);
+
 function proxyTest(cachedUserRepository) {
-  var allUsers, user;
+    var allUsers, user;
 
-  cachedUserRepository.findAll(function(users) {
-    console.log("should send AJAX request");
-    assertEqual(users.length, 3);
-  });
-
-  setTimeout(function() {
-    cachedUserRepository.findAll(function(users) {
-      console.log("should return cached results");
-      assertEqual(users.length, 3);
+    cachedUserRepository.findAll(function (users) {
+        console.log("should send AJAX request", users);
+        assertEqual(users.length, 3);
     });
-  }, 3000);
 
-  setTimeout(function() {
-    cachedUserRepository.findAll(function(users) {
-      console.log("should send new AJAX request");
-      assertEqual(users.length, 3);
-    });
-  }, 6000);
+    setTimeout(function () {
+        cachedUserRepository.findAll(function (users) {
+            console.log("should return cached results", users);
+            assertEqual(users.length, 3);
+        });
+    }, 3000);
+
+    setTimeout(function () {
+        cachedUserRepository.findAll(function (users) {
+            console.log("should send new AJAX request", users);
+            assertEqual(users.length, 3);
+        });
+    }, 6000);
 }
+
+function proxyTestById(cachedUserRepository) {
+    var allUsers, user;
+
+    cachedUserRepository.findById(2, function (users) {
+        console.log("should send AJAX request", users);
+        assertEqual(users.name, 'Batman');
+    });
+
+    setTimeout(function () {
+        cachedUserRepository.findById(2, function (users) {
+            console.log("should return cached results", users);
+            assertEqual(users.name, 'Batman');
+        });
+        cachedUserRepository.findById(3, function (users) {
+            console.log("should return ajax results", users);
+            assertEqual(users.name, 'Jacykow');
+        });
+    }, 3000);
+
+    setTimeout(function () {
+        cachedUserRepository.findById(2, function (users) {
+            console.log("should send new AJAX request", users);
+            assertEqual(users.name, 'Batman');
+        });
+    }, 6000);
+}
+
+console.log('proxyTest');
+proxyTest(cachedUserRepository);
+console.log('//proxyTest');
+
+setTimeout(function () {
+    console.log('proxyTestById');
+    proxyTestById(cachedUserRepository);
+    console.log('//proxyTestById');
+}, 8000);
--- js/dp/Stateful.js
+++ js/dp/Stateful.js
@@ -1,21 +1,58 @@
 // Implement Stateful role (Singleton)
 // Reuse: mixin and Evented role
-function statefulExampleUsage(mixin, Stateful) {
-  var bob = {};
-  mixin(bob, Stateful);
 
-  bob.watch("name", function(oldValue, newValue) {
-    log("name changed from:", oldValue, "to:", newValue);
-  });
-  bob.watch("birthDate", function(oldValue, newValue) {
-    log("birthDate changed from:", oldValue, "to:", newValue);
-  });
-  bob.watchAll(function(attributeName, oldValue, newValue) {
-    log("Attribute:", attributeName, " changed:", oldValue, newValue);
-  });
-  bob.set("name", 123);
-  bob.set("birthDate", new Date());
-  bob.get("name");
+
+//stateful pattern
+var Stateful = (function (mixin, Evented) {
+
+    var statefull = {
+        watch: function (name, fn) {
+            this.on(name, fn);
+        },
+        watchAll: function (fn) {
+            this.on('watchall', fn);
+        },
+        set: function (property, value) {
+            var old = this[property]
+            this[property] = value;
+            this.trigger(property, [old, value]);
+            this.trigger('watchall', [property, old, value]);
+        },
+        get: function (property) {
+            return this[property];
+        },
+        afterMixed:function(){
+            //reset events hash, if not we share it among all object after mixin
+            this.events = {};
+        }
+    }
+
+    mixin(statefull, Evented);
+
+    return statefull;
+})(mixin, Evented);
+
+function statefulExampleUsage(mixin, Stateful) {
+    var bob = {};
+    mixin(bob, Stateful);
+
+    bob.watch("name", function (oldValue, newValue) {
+        log("name changed from:", oldValue, "to:", newValue);
+    });
+    bob.watch("birthDate", function (oldValue, newValue) {
+        log("birthDate changed from:", oldValue, "to:", newValue);
+    });
+    bob.watchAll(function (attributeName, oldValue, newValue) {
+        log("Attribute:", attributeName, " changed:", oldValue, newValue);
+    });
+    bob.set("name", 123);
+    bob.set("birthDate", new Date());
+    console.log(bob.get("name"), bob);
 
 //  bob.toJSON();
 }
+
+console.log('statefulExampleUsage');
+console.log('======================');
+//statefulExampleUsage(mixin, Stateful);
+console.log('//statefulExampleUsage');
