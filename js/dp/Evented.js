d49881d5155a1f48b6ffa4fe446f87e58762b6e7
Adam Gawelczyk <adam.gawelczyk@motorolasolutions.com>
2014-07-16 11:17:05 +0200

    Evented ready - events implemented

--- js/dp/Evented.js
+++ js/dp/Evented.js
@@ -3,25 +3,63 @@
 // + trigger(eventName, args)
 // + off(eventName)
 
+
+var Evented = (function () {
+
+    function on(eventName, listenerFn) {
+        if (!this.events[eventName])
+            this.events[eventName] = [];
+        this.events[eventName].push(listenerFn);
+    }
+
+    function trigger(eventName, args) {
+        if (this.events[eventName]) {
+            for (var i = 0; i < this.events[eventName].length; i++) {
+                this.events[eventName][i].apply(this, args);
+            }
+        }
+    }
+
+    function off(eventName) {
+        if (this.events[eventName])
+            delete this.events[eventName]
+    }
+
+    return {
+        on: on,
+        trigger: trigger,
+        off: off,
+        afterMixed: function () {
+            this.events = {};
+        }
+    }
+
+})();
+
 function eventedExampleUsage(mixin, Evented) {
-  var bob = {name: "Bob"};
-  mixin(bob, Evented);
+    var bob = {name: "Bob"};
+    mixin(bob, Evented);
 
-  bob.on("remove", function(number, str) {
-    log(this.name, "remove triggered", arguments);
-  });
+    bob.on("remove", function (number, str) {
+        log(this.name, "remove triggered", arguments);
+    });
 
-  bob.on("remove", function() {
-    log(this.name, "2nd remove triggered", arguments);
-  });
+    bob.on("remove", function () {
+        log(this.name, "2nd remove triggered", arguments);
+    });
 
-  bob.trigger("remove", [1, "qq"]); // logged msg on console
+    bob.trigger("remove", [1, "qq"]); // logged msg on console
 
-  var ed = {name: "Ed"};
-  mixin(ed, Evented);
-  ed.trigger("remove"); // nothing happens
+    var ed = {name: "Ed"};
+    mixin(ed, Evented);
+    ed.trigger("remove"); // nothing happens
 
-  bob.off("remove");
-  bob.trigger("remove", [{}, 997.001]); // nothing logged
+    bob.off("remove");
+    bob.trigger("remove", [
+        {},
+        997.001
+    ]); // nothing logged
 }
-//eventedExampleUsage(mixin, Evented);
+console.log('eventedExampleUsage');
+eventedExampleUsage(mixin, Evented);
+console.log('//eventedExampleUsage');
