6669d437c627ad1afcb170e4c434a58b1b061079
Adam Gawelczyk <adam.gawelczyk@motorolasolutions.com>
2014-07-16 15:12:31 +0200

    repository implemented

--- js/dp/Repository.js
+++ js/dp/Repository.js
@@ -6,20 +6,54 @@
 //  console.log("Server error", error);
 //});
 
+var UsersRepository = (function () {
+
+    var findAll = function (fn) {
+        var promise = $.get("server/users.json"); // AJAX request
+        promise.success(function (data) {
+            fn(data);
+        });
+        promise.error(function (error) {
+            console.log("Server error", error);
+        });
+    };
+
+    var findById = function (id, fn) {
+        var promise = $.get("server/users.json"); // AJAX request
+        promise.success(function (data) {
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].id == id) {
+                    fn(data[i]);
+                    return;
+                }
+            }
+            fn();
+        });
+        promise.error(function (error) {
+            console.log("Server error", error);
+        });
+    };
+
+    return {
+        findAll: findAll,
+        findById: findById
+    }
+})();
 function repositoryTest(usersRepository) {
-  var allUsers, user;
+    var allUsers, user;
 
-  usersRepository.findAll(function(users) {
-    assertEqual(users.length, 3);
-  });
+    usersRepository.findAll(function (users) {
+        assertEqual(users.length, 3);
+    });
 
-  usersRepository.findById(2, function(user) {
-    assertEqual(user.name, "Batman");
-  });
+    usersRepository.findById(2, function (user) {
+        assertEqual(user.name, "Batman");
+    });
 
-  usersRepository.findById(997, function(user) {
-    assertEqual(user, undefined);
-  });
+    usersRepository.findById(997, function (user) {
+        assertEqual(user, undefined);
+    });
 }
-
-//repositoryTest(usersRepository);
+console.log('repositoryTest');
+repositoryTest(UsersRepository);
+console.log('//repositoryTest');
